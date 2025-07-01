c97563d0c163e1342b72650d8ce533636397abe2
AF2310 <aferat321@gmail.com>
2025-07-01 10:51:19 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,11 +1,11 @@
 import ubinascii
 import machine
-WIFI_SSID = "TN-RH5910"
-WIFI_PASS = "nidraHabyet5"
+WIFI_SSID = ""
+WIFI_PASS = ""
 AIO_SERVER = "io.adafruit.com"
 AIO_PORT = 1883
 AIO_USER = "aferat"
-AIO_KEY = "aio_gJgQ34NyTIBH28kTqiiaweQSoNkC"
+AIO_KEY = "aio_"
 AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id())  # Can be anything
 AIO_BUZZER_FEED = "aferat/feeds/alarm-setting"
 AIO_PIR_FEED = "aferat/feeds/motion-detection"
