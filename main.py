807b5e111cc30d42e9bcfdfb944aa5f3a91ffce8
AF2310 <aferat321@gmail.com>
2025-07-04 10:52:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,19 @@ buzzer.duty_u16(0)
 prev_tilt = tilt_switch.value()
 tilt_triggered = False
 alarm_enabled = True
-prev_alarm = None 
+prev_alarm = None
+
+I_PICO = 0.050
+I_WIFI_IDLE = 0.090
+I_WIFI_ACTIVE = 0.100
+I_PIR_IDLE = 0.00005
+I_PIR_ACTIVE = 0.065
+I_HALL = 0.005
+I_BUZZER = 0.040
+VOLTAGE = 5
+
+idle_seconds = 0
+triggered_seconds = 0
 
 client = MQTTClient("alarm-device", keys.AIO_SERVER, keys.AIO_PORT, keys.AIO_USER, keys.AIO_KEY)
 
@@ -38,6 +50,63 @@ def send_feed(feed, value):
         client.publish(feed, str(value))
     except Exception as e:
         print(f"MQTT error on {feed}:", e)
+        
+def calculate_power_usage():
+    idle_hours = idle_seconds / 3600
+    triggered_hours = triggered_seconds / 3600
+    
+    power_pico = VOLTAGE * I_PICO
+    power_wifi_idle = VOLTAGE * I_WIFI_IDLE
+    power_wifi_active = VOLTAGE * I_WIFI_ACTIVE
+    power_pir_idle = VOLTAGE * I_PIR_IDLE
+    power_pir_active = VOLTAGE * I_PIR_ACTIVE
+    power_hall = VOLTAGE * I_HALL
+    power_buzzer = VOLTAGE * I_BUZZER
+    
+    avg_power_wifi = (power_wifi_idle * idle_seconds + power_wifi_active * triggered_seconds) / (idle_seconds + triggered_seconds + 1e-6)
+    avg_power_pir = (power_pir_idle * idle_seconds + power_pir_active * triggered_seconds) / (idle_seconds + triggered_seconds + 1e-6)
+    
+    total_power = power_pico + avg_power_wifi + avg_power_pir + power_hall + power_buzzer
+    
+    
+    
+    
+    energy_pico = power_pico * (idle_hours + triggered_hours)
+    energy_wifi = (power_wifi_idle * idle_hours) + (power_wifi_active * triggered_hours)
+    energy_pir = (power_pir_idle * idle_hours) + (power_pir_active * triggered_hours)
+    energy_hall = power_hall * triggered_hours
+    energy_buzzer = power_buzzer * triggered_hours
+    
+    
+    
+    total_energy = energy_pico + energy_wifi + energy_pir + energy_hall + energy_buzzer
+    
+    
+    print(f"Pico Power:      {power_pico:.3f} W")
+    print(f"WiFi Idle Power: {power_wifi_idle:.3f} W")
+    print(f"WiFi Active:     {power_wifi_active:.3f} W")
+    print(f"PIR Idle:        {power_pir_idle:.5f} W")
+    print(f"PIR Active:      {power_pir_active:.3f} W")
+    print(f"Hall Sensor:     {power_hall:.3f} W")
+    print(f"Buzzer:          {power_buzzer:.3f} W")
+    print(f"→ Total Power:    {total_power:.3f} W")
+    
+    print(f"Idle time:      {idle_hours:.2f} h")
+    print(f"Triggered time: {triggered_hours:.2f} h\n")
+    print(f"Pico:       {energy_pico:.2f} Wh")
+    print(f"WiFi:           {energy_wifi:.2f} Wh")
+    print(f"PIR:     {energy_pir:.2f} Wh")
+    print(f"Hall:    {energy_hall:.2f} Wh")
+    print(f"Buzzer:         {energy_buzzer:.2f} Wh")
+    
+    print(f"Total:          {total_energy:.2f} Wh")
+    
+    send_feed(keys.AIO_POWER_PICO_FEED, round(energy_pico, 3))
+    send_feed(keys.AIO_POWER_WIFI_FEED, round(energy_wifi, 3))
+    send_feed(keys.AIO_POWER_PIR_FEED, round(energy_pir, 3))
+    send_feed(keys.AIO_POWER_HALL_FEED, round(energy_hall, 3))
+    send_feed(keys.AIO_POWER_BUZZER_FEED, round(energy_buzzer, 3))
+    send_feed(keys.AIO_POWER_TOTAL_FEED, round(total_energy, 3))
 
 client.set_callback(sub_cb)
 client.connect()
@@ -45,7 +114,7 @@ client.subscribe(keys.AIO_BUZZER_FEED)
 print("MQTT connected and subscribed.")
 
 def alarm_loop():
-    global alarm_enabled, prev_alarm
+    global alarm_enabled, prev_alarm , idle_seconds, triggered_seconds
     pir_prev = 0
     hall_prev = 1
     #prev_alarm = None
@@ -56,6 +125,7 @@ def alarm_loop():
     prev_tilt = tilt_switch.value()
     last_tilt_change = time.ticks_ms()
     tilt_debounce_ms = 300
+    last_time = time.time() 
 
     while True:
         try:
@@ -101,6 +171,7 @@ def alarm_loop():
                     buzzer.duty_u16(3000)
                     alarm_state = "1"
                     buzz_timer += 1
+                    triggered_seconds += 0.2
                     if buzz_timer >= buzz_interval:
                         send_feed(keys.AIO_ALARM_FEED, alarm_state)
                         buzz_timer = 0
@@ -108,13 +179,20 @@ def alarm_loop():
                     buzzer.duty_u16(0)
                     alarm_state = "0"
                     buzz_timer = 0
+                    idle_seconds += 0.2
             else:
                 buzzer.duty_u16(0)
                 alarm_state = "0"
                 buzz_timer = 0
+                idle_seconds += 0.2
+
             if alarm_state != prev_alarm :
                 send_feed(keys.AIO_ALARM_FEED, alarm_state)
                 prev_alarm = alarm_state
+            
+            if time.time() - last_time >= 30:
+                calculate_power_usage()
+                last_time = time.time()
 
         except Exception as e:
             print("Loop error:", e)
